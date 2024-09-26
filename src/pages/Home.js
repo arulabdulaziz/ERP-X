38724612d34535a1890483848b0c21c2fa1c0d09
arulabdulaziz <rulz.matrixs@gmail.com>
2024-09-26 11:52:48 +0700

    init tailwindcss

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <nav>
-        <Link to="/user">User</Link> | <Link to="/client">Client</Link> | <Link to="/project">Project</Link>
+        <Link to="/user">User</Link> | <Link to="/client">Client</Link> |{" "}
+        <Link to="/project">Project</Link>
       </nav>
+      <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white">
+        <h1 className="text-4xl font-bold cursor-pointer">Hello, Tailwind CSS!</h1>
+      </div>
     </div>
   );
 };
